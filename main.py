9fd340152c1e7ace7c6f97e054d5e4ed8fd9d14b
Tofu Gang <tofugangsw@gmail.com>
2020-12-09 21:38:43 +0100

    Day 9: Puzzles 1 and 2 solved.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 __author__ = "Tofu Gang"
 __email__ = "tofugangsw@gmail.com"
 
-from day_08.day_08 import puzzle_2 as puzzle
+from day_09.day_09 import puzzle_2 as puzzle
 
 ################################################################################
 
